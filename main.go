7dee3ba59c1db6d8d6f50e007893cf0543d9ae2b
Jeff Qian <jeff.qian@gmail.com>
2017-07-18 00:24:26 +0800

    add vendor

--- main.go
+++ main.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-	"github.com/kataras/iris"
-	"github.com/kataras/iris/context"
+	"os"
+
+	"github.com/urfave/cli"
+
+	"github.com/dancewing/go-task/cmd"
+	"github.com/dancewing/go-task/pkg/setting"
 )
 
+const APP_VER = "0.11.24.0627"
+
+func init() {
+	setting.AppVer = APP_VER
+}
+
 func main() {
-	app := iris.New()
-
-	app.Config.IsDevelopment = true
-
-	// Load all templates from the "./templates" folder
-	// where extension is ".html" and parse them
-	// using the standard `html/template` package.
-	app.RegisterView(iris.HTML("./templates", ".html"))
-	//app.RegisterView(iris.BASIC("./templates", ".tmpl"))
-
-	// Method:    GET
-	// Resource:  http://localhost:8080
-	app.Get("/", func(ctx context.Context) {
-		// Bind: {{.message}} with "Hello world!"
-		ctx.ViewData("message", "Hello world!")
-		// Render template file: ./templates/hello.html
-		ctx.View("hello.html")
-	})
-
-	// Start the server using a network address and block.
-	app.Run(iris.Addr(":8080"))
+	app := cli.NewApp()
+	app.Name = "Gogs"
+	app.Usage = "A painless self-hosted Git service"
+	app.Version = APP_VER
+	app.Commands = []cli.Command{
+		cmd.Web,
+	}
+	app.Flags = append(app.Flags, []cli.Flag{}...)
+	app.Run(os.Args)
 }
